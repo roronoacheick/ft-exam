a5278b07018d76686f33d5f58aa31ce2bb709186
roronoacheick <roronoacheick3@icloud.com>
2025-06-23 17:20:24 +0200

    feat(sevr.js):add /random endpoint returning a random number

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
     res.json({ message: 'hello world' });
 });
 
+app.get('/random', (req, res) => {
+    const randomInt = Math.floor(Math.random() * 1000000);
+    res.json({ random: randomInt });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-}); 
+});
+
+// Remove me
